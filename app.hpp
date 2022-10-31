eb909dba9b5a75d31ebdb0f5c5c0714a5de74f68
pierrick.martin <pierrick@mart1.dev>
2022-10-31 23:02:43 +0100

    02 - Graphics Pipeline Overview

--- app.hpp
+++ app.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "lve_window.hpp"
+#include "lve_pipeline.hpp"
 
 namespace lve {
     class App {
@@ -12,5 +13,6 @@ namespace lve {
 
         private:
             LveWindow lveWindow{WIDTH, HEIGHT, "Vulkan Engine"};
+            LvePipeline lvePipeline{"shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv"};
     };
 }
