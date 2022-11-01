cac0ad1deb750ebeb9209c12d09d3eeccd0f8337
pierrick.martin <pierrick@mart1.dev>
2022-11-01 17:42:01 +0100

    05 - Swap Chain Overview

--- app.cpp
+++ app.cpp
@@ -1,9 +1,55 @@
 #include "app.hpp"
 
-namespace lve {
-    void App::run() {
-        while (!lveWindow.shouldClose()) {
+#include <stdexcept>
+
+namespace lve
+{
+    App::App()
+    {
+        createPipelineLayout();
+        createPipeline();
+        createCommandBuffers();
+    }
+
+    App::~App()
+    {
+        vkDestroyPipelineLayout(lveDevice.device(), pipelineLayout, nullptr);
+    }
+
+    void App::run()
+    {
+        while (!lveWindow.shouldClose())
+        {
             glfwPollEvents();
         }
     }
+
+    void App::createPipelineLayout()
+    {
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+
+        pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipelineLayoutInfo.setLayoutCount = 0;
+        pipelineLayoutInfo.pSetLayouts = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 0;
+        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+
+        if (vkCreatePipelineLayout(lveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS)
+        {
+            throw std::runtime_error("cannot create pipeline layout");
+        }
+    }
+
+    void App::createPipeline()
+    {
+        auto pipelineConfig = LvePipeline::defaultPipelineConfigInfo(lveSwapChain.width(), lveSwapChain.height());
+
+        pipelineConfig.renderPass = lveSwapChain.getRenderPass();
+        pipelineConfig.pipelineLayout = pipelineLayout;
+        lvePipeline = std::make_unique<LvePipeline>(
+            lveDevice, "shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv", pipelineConfig);
+    }
+
+    void App::createCommandBuffers() {}
+    void App::drawFrame() {}
 }
--- app.hpp
+++ app.hpp
@@ -3,18 +3,37 @@
 #include "lve_window.hpp"
 #include "lve_pipeline.hpp"
 #include "lve_device.hpp"
+#include "lve_swap_chain.hpp"
 
-namespace lve {
-    class App {
-        public:
-            static constexpr int WIDTH = 1600;
-            static constexpr int HEIGHT = 1200;
+#include <memory>
 
-            void run();
+namespace lve
+{
+    class App
+    {
+    public:
+        static constexpr int WIDTH = 1600;
+        static constexpr int HEIGHT = 1200;
 
-        private:
-            LveWindow lveWindow{WIDTH, HEIGHT, "Vulkan Engine"};
-            LveDevice lveDevice{lveWindow};
-            LvePipeline lvePipeline{lveDevice, "shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv", LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
+        App();
+        ~App();
+
+        App(const App &) = delete;
+        App &operator=(const App &) = delete;
+
+        void run();
+
+    private:
+        void createPipelineLayout();
+        void createPipeline();
+        void createCommandBuffers();
+        void drawFrame();
+
+        LveWindow lveWindow{WIDTH, HEIGHT, "Vulkan Engine"};
+        LveDevice lveDevice{lveWindow};
+        LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtend()};
+        std::unique_ptr<LvePipeline> lvePipeline;
+        VkPipelineLayout pipelineLayout;
+        std::vector<VkCommandBuffer> commandBuffers;
     };
 }
