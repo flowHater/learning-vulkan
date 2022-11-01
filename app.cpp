4a6d2f542f05ae8d1fc2a7005be53627903524d4
pierrick.martin <pierrick@mart1.dev>
2022-11-01 23:10:19 +0100

    06 - Vertex Buffer

--- app.cpp
+++ app.cpp
@@ -7,6 +7,7 @@ namespace lve
 {
     App::App()
     {
+        loadModels();
         createPipelineLayout();
         createPipeline();
         createCommandBuffers();
@@ -26,6 +27,16 @@ namespace lve
         }
 
         vkDeviceWaitIdle(lveDevice.device());
+    }
+
+    void App::loadModels() {
+        std::vector<LveModel::Vertex> vertices {
+            {{0.0f, -0.5f}},
+            {{0.5f, 0.5f}},
+            {{-0.5f, 0.5f}}
+        };
+
+        lveModel = std::make_unique<LveModel>(lveDevice, vertices);
     }
 
     void App::createPipelineLayout()
@@ -94,9 +105,10 @@ namespace lve
             renderPassInfo.pClearValues = clearValues.data();
 
             vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+            
             lvePipeline->bind(commandBuffers[i]);
-            vkCmdDraw(commandBuffers[i], 3, 1, 0 , 0);
-
+            lveModel->bind(commandBuffers[i]);
+            lveModel->draw(commandBuffers[i]);
             vkCmdEndRenderPass(commandBuffers[i]);
 
             if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
