b849af18f10d251da348c2e4582685f02c0c474e
Pierrick Martin <pierrick@mart1.dev>
2023-09-24 01:15:26 +0200

    list monitor and used the primary one

--- src/lve_window.cpp
+++ src/lve_window.cpp
@@ -1,4 +1,6 @@
 #include "lve_window.hpp"
+#include <GLFW/glfw3.h>
+#include <iostream>
 
 namespace lve
 {
@@ -24,6 +26,28 @@ namespace lve
             throw "glfwInit()";
         }
 
+        int countMonitor = 0;
+
+        auto monitors = glfwGetMonitors(&countMonitor);
+        if (monitors == nullptr)
+        {
+            throw "glfwGetMonitors()";
+        }
+        for (int i = 0; i < countMonitor; i++)
+        {
+            std::cout << "Monitor #" << i << "= " << glfwGetMonitorName(monitors[i]) << std::endl;
+        }
+
+        auto monitor = glfwGetPrimaryMonitor();
+        auto mode = glfwGetVideoMode(monitor);
+        std::cout << "Monitor " << glfwGetMonitorName(monitor) << " will be used." << std::endl;
+
+        glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+        glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+        glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+        glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+        glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
@@ -32,7 +56,7 @@ namespace lve
 
         glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
 
-        window = glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
+        window = glfwCreateWindow(width, height, windowName.c_str(), monitor, nullptr);
         glfwSetWindowUserPointer(window, this);
         glfwSetFramebufferSizeCallback(window, frameBufferResizedCallback);
     }
